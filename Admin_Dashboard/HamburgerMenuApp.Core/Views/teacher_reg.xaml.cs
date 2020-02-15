2d8e2dd367cdc64280ed3c8991f011ac63ce7e32
abhilash-seizecode <abhilash.mn2604@gmail.com>
2020-02-15 15:37:49 +0530

    Updated for teacher registration

--- Admin_Dashboard/HamburgerMenuApp.Core/Views/teacher_reg.xaml.cs
+++ Admin_Dashboard/HamburgerMenuApp.Core/Views/teacher_reg.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace HamburgerMenuApp.Core.Views
 {
@@ -24,15 +26,41 @@ namespace HamburgerMenuApp.Core.Views
         {
             InitializeComponent();
         }
-
+        string constring = ConfigurationManager.AppSettings["Constring"];
         private void Btn_clear_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("test");
+            teacher_name.Text = string.Empty;
+            staff_id.Text = string.Empty;
+            //class_dept.Text = string.Empty;
+            ph_no.Text = string.Empty;
+            email.Text = string.Empty;
+            address.Text = string.Empty;
+            dob.Text = string.Empty;
         }
 
         private void Btn_add_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                MySqlConnection con = new MySqlConnection(constring);
+                con.Open();
+                string query = "insert into teacher_register(teach_id,password,teach_name,dept,desig,ph_no,email,address,gender,dob) values ('" + staff_id.Text + "','" + getpassword() + "','" + teacher_name.Text + "','" + department.Text + "','" + Designation.Text + "','" + ph_no.Text + "','" + email.Text + "','" + address.Text + "','" + gender.Text + "','" + dob.Text + "')";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Teacher Detail added sucessfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
+        private string getpassword()
+        {
+            int lengthOfPassword = 8;
+            string passCode = DateTime.Now.Ticks.ToString();
+            string pass = BitConverter.ToString(new System.Security.Cryptography.SHA512CryptoServiceProvider().ComputeHash(Encoding.Default.GetBytes(passCode))).Replace("-", String.Empty);
+            return pass.Substring(0, lengthOfPassword);
         }
     }
 }
