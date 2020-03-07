d4afb188f898980a785c667b74344d199f8f4f93
abhilash-seizecode <abhilash.mn2604@gmail.com>
2020-03-07 17:26:44 +0530

    Database updated !

--- Database/collegedb.sql
+++ Database/collegedb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2020 at 01:01 PM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.1.33
+-- Generation Time: Mar 07, 2020 at 12:56 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,76 @@ SET time_zone = "+00:00";
 --
 -- Database: `collegedb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignments`
+--
+
+CREATE TABLE `assignments` (
+  `id` int(10) NOT NULL,
+  `dept` varchar(50) NOT NULL,
+  `sem` varchar(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `_desc` varchar(1000) NOT NULL,
+  `start_date` varchar(50) NOT NULL,
+  `end_date` varchar(50) NOT NULL,
+  `assigned_by` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `assignments`
+--
+
+INSERT INTO `assignments` (`id`, `dept`, `sem`, `name`, `_desc`, `start_date`, `end_date`, `assigned_by`) VALUES
+(1, 'BCA', '1', 'asdasd a', 'adasdasdasd asd asda sdasda', '12-12-2009', '12-12-2009', 'asdasj das'),
+(2, 'BCA', '6', 'adsadsad', 'adfdasfsafsadfsadf', '12-12-2020', '12-12-2021', 'asdasdasdasd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(10) NOT NULL,
+  `dept` varchar(50) NOT NULL,
+  `sem` varchar(10) NOT NULL,
+  `event_name` varchar(255) NOT NULL,
+  `_desc` varchar(1000) NOT NULL,
+  `organised_by` varchar(255) NOT NULL,
+  `from_date` varchar(50) NOT NULL,
+  `to_date` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `dept`, `sem`, `event_name`, `_desc`, `organised_by`, `from_date`, `to_date`) VALUES
+(2, 'BCA', '1', 'sadfsadjfkh', 'sadfasdf', 'sadfasdf', '15-03-2020', '27-03-2020'),
+(3, 'BBM', '2', 'asdasd', 'asd', 'asdasd', '07-03-2020', '08-03-2020');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leaves`
+--
+
+CREATE TABLE `leaves` (
+  `id` int(10) NOT NULL,
+  `dept` varchar(50) NOT NULL,
+  `sem` varchar(10) NOT NULL,
+  `leave_to` varchar(75) NOT NULL,
+  `leave_from` varchar(75) NOT NULL,
+  `subject` varchar(250) NOT NULL,
+  `reason` varchar(1000) NOT NULL,
+  `from_date` varchar(50) NOT NULL,
+  `to_date` varchar(50) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT '0',
+  `faculty_message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -45,6 +115,28 @@ INSERT INTO `login` (`id`, `username`, `password`, `email`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `qpaper`
+--
+
+CREATE TABLE `qpaper` (
+  `id` int(10) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `_desc` varchar(1000) NOT NULL,
+  `dept` varchar(50) NOT NULL,
+  `sem` varchar(10) NOT NULL,
+  `url` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `qpaper`
+--
+
+INSERT INTO `qpaper` (`id`, `title`, `_desc`, `dept`, `sem`, `url`) VALUES
+(2, 'sajdh', 'asdasdasd', 'BCA', '4', 'http://localhost/phpmyadmin/tbl_structure.php?db=collegedb&table=qpaper');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `std_register`
 --
 
@@ -53,8 +145,9 @@ CREATE TABLE `std_register` (
   `name` varchar(255) NOT NULL,
   `reg_no` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `class` varchar(255) NOT NULL,
+  `dept` varchar(255) NOT NULL,
   `sem` varchar(255) NOT NULL,
+  `designation` varchar(100) NOT NULL,
   `ph_no` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `address` varchar(755) NOT NULL,
@@ -66,47 +159,45 @@ CREATE TABLE `std_register` (
 -- Dumping data for table `std_register`
 --
 
-INSERT INTO `std_register` (`id`, `name`, `reg_no`, `password`, `class`, `sem`, `ph_no`, `email`, `address`, `gender`, `dob`) VALUES
-(7, 'abhilash', '1002', 'F7BCD141', 'BCA', '2', '142141', 'asdas@gmail.com', 'asdkakhsd ashdajsd', 'Male', 'adas'),
-(8, 'bharathi', '1003', '037310E3', 'BCA', '2', '9127389161', 'sadh@gmail.com', 'hasdgahs hasgdahskgd', 'Female', '12-12-1990'),
-(9, 'kasdhajksh', '1004', '49428CB2', 'BCA', '2', '9120387123', 'ashdads@gmail.com', 'uasdksahdjash', 'Female', '12-12-1999'),
-(10, 'jasdghha', '1005', 'B4F0A04E', 'BBM', '1', '989856556', 'ajdhkjsa@gmail.com', 'ajkdhajksdh', 'Female', '12-09-1989');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teacher_register`
---
-
-CREATE TABLE `teacher_register` (
-  `id` int(10) NOT NULL,
-  `teach_id` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `teach_name` varchar(255) NOT NULL,
-  `dept` varchar(255) NOT NULL,
-  `desig` varchar(255) NOT NULL,
-  `ph_no` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `address` varchar(755) NOT NULL,
-  `gender` varchar(50) NOT NULL,
-  `dob` varchar(75) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `teacher_register`
---
-
-INSERT INTO `teacher_register` (`id`, `teach_id`, `password`, `teach_name`, `dept`, `desig`, `ph_no`, `email`, `address`, `gender`, `dob`) VALUES
-(1, '112', 'A5847A40', 'abc', 'commerce', 'HOD', '998650651', 'asdas@gmail.com', 'akhdaksh kashdk jasjkdhajkhdjk ahdjkashdjkashjkdasjkdhajksdhajksdhkjahd asjkdh ajkhdjka skjdha kjhd', 'Male', '12-12-1990');
+INSERT INTO `std_register` (`id`, `name`, `reg_no`, `password`, `dept`, `sem`, `designation`, `ph_no`, `email`, `address`, `gender`, `dob`) VALUES
+(1, 'Bharathi', '1001', 'abc', 'BCA', '4', 'student', '0980912737', 'bharathidhanu231@gmail.com', 'akjdhas jadhjkl ajklsdhaj ', 'Female', '12-12-1997'),
+(2, 'ajkahsdj ajsdhjadh', '1002', '5D1C2CB9', 'BCA', '3', 'student', '98965656565', 'ahdjad@gmail.com', 'hasgdkahgdhagd', 'Male', '12-12-1990'),
+(6, 'asdkh', '2002', 'F9DCD17B', 'BBM', '2', 'faculty', '0981230978', 'ajksdh@gmail.com', 'akshdaksjdh', 'Male', '12-12-1990'),
+(7, 'adfsasffd', '2001', '44EBC7D3', 'BBM', '4', 'faculty', '0981230978', 'ajksdh@gmail.com', 'akshdaksjdh', 'Male', '12-12-1990'),
+(8, 'adasd', '1003', '1C981977', 'BCA', '3', 'student', '0984656545', 'asda@gmail.com', 'adahsg khadkha skhdakd', 'Male', '31-12-1998');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `leaves`
+--
+ALTER TABLE `leaves`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `qpaper`
+--
+ALTER TABLE `qpaper`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -116,14 +207,26 @@ ALTER TABLE `std_register`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `teacher_register`
---
-ALTER TABLE `teacher_register`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `assignments`
+--
+ALTER TABLE `assignments`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `leaves`
+--
+ALTER TABLE `leaves`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `login`
@@ -132,16 +235,16 @@ ALTER TABLE `login`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `qpaper`
+--
+ALTER TABLE `qpaper`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `std_register`
 --
 ALTER TABLE `std_register`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `teacher_register`
---
-ALTER TABLE `teacher_register`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
